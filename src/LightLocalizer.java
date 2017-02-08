65ffd83f792a7820fbffbab6ad35dcaeca668155
nevmx <maxsnev@gmail.com>
2017-02-07 23:25:57 -0500

    Light localization done

--- src/LightLocalizer.java
+++ src/LightLocalizer.java
@@ -1,7 +1,10 @@
+import lejos.hardware.Sound;
 import lejos.robotics.SampleProvider;
 
 public class LightLocalizer {
-	public static int ROTATION_SPEED = 60;
+	private static final double COLOR_SENSOR_RADIUS = 10;
+	private static final int ROTATION_SPEED = 60;
+	private static final double COLOR_SENSOR_BOUND = 0.40;
 	
 	private Odometer odo;
 	private SampleProvider colorSensor;
@@ -25,9 +28,61 @@ public class LightLocalizer {
 		double finalTheta = 0.0;
 		
 		// start rotating and clock all 4 gridlines
+		navigation.setSpeeds(-ROTATION_SPEED, ROTATION_SPEED);
+		int clockedLines = 0;
 		
+		boolean sensorAboveLine = false;
+		
+		double angleOne = 0.0;
+		double angleTwo = 0.0;
+		
+		double[] angles = new double[4];
+		
+		while (clockedLines < 4) {
+			// Get the color reading
+			colorSensor.fetchSample(this.colorData, 0);
+
+			if (this.colorData[0] < this.COLOR_SENSOR_BOUND && !sensorAboveLine) {
+				sensorAboveLine = true;
+				angleOne = odo.getAng();
+			}
+			
+			if (this.colorData[0] > this.COLOR_SENSOR_BOUND && sensorAboveLine) {
+				angleTwo = odo.getAng();
+				angles[clockedLines++] = (angleOne + angleTwo)/2.0;
+				sensorAboveLine = false;
+				Sound.beep();
+			}
+		}
+		
+		navigation.setSpeeds(0, 0);
 		
 		// do trig to compute (0,0) and 0 degrees
+		// X
+		double thetaX = Math.abs(angles[0] - angles[2]);
+		
+		if (thetaX > 180) {
+			thetaX = 360 - thetaX;
+		}
+		
+		finalX = -COLOR_SENSOR_RADIUS * Math.cos(Math.toRadians(thetaX / 2.0));
+		
+		// Y
+		double thetaY = Math.abs(angles[1] - angles[3]);
+		
+		if (thetaY > 180) {
+			thetaY = 360 - thetaY;
+		}
+		
+		finalY = -COLOR_SENSOR_RADIUS * Math.cos(Math.toRadians(thetaY / 2.0));
+		
+		// Theta
+		double deltaTheta = 270 + (thetaX/2.0) - angles[0];
+		finalTheta = odo.getAng() + deltaTheta;
+		
+		if (finalTheta > 180.0) {
+			finalTheta -= 180.0;
+		}
 		
 		odo.setPosition(new double[] {finalX, finalY, finalTheta}, new boolean[] {true, true, true});
 		// when done travel to (0,0) and turn to 0 degrees
